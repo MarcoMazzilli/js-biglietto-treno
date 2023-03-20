72742965e784a409facb5593657209e1c6727964
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-03-20 14:38:56 +0100

    [Add] Basic function for ticket price. (no math)

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 //Dati da richiedere
-const age =parseInt( prompt("Inserisci la tua età", 23));
+const age =parseInt( prompt("Inserisci la tua età", 50));
 
 const kmDaPercorrere =parseFloat(prompt("Quanti Km dovrai percorrere?", 30));
 
@@ -13,5 +13,15 @@ const discountOver = 40;
 //Risultati delle operazioni
 let prezzoLordo = kmDaPercorrere * prezzoAlKm;
 console.log(prezzoLordo);
+
+if (age <= 18){
+  prezzoNetto = prezzoLordo - (prezzoLordo * discountUnder / 100);
+}else if (age >= 65){
+  prezzoNetto = prezzoLordo - (prezzoLordo * discountOver / 100);
+}else {
+  prezzoNetto = prezzoLordo
+}
+
+console.log(prezzoNetto)
 
 
